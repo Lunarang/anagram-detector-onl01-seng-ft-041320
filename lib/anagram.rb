a3e775364939ed6063b87cd6258967414cf5e2af
Erin Mettille <ecmettille@gmail.com>
2020-04-27 00:44:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,14 +2,14 @@ class Anagram
   attr_accessor :word 
   
   def initialize (word)
-    @word = word.split("")
+    @word = word.split("").sort
   end
 
   def match (array)
     
     array.each do |anagram|
       anagram = anagram.split("")
-      if @word.sort == anagram.sort 
+      if @word == anagram.sort 
         anagram
       else
         array.clear
